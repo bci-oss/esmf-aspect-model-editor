b950919236e7516c1c2e7429afb0b0526d12cb32
Moscu George <moscugeorge@gmail.com>
2022-08-11 18:10:10 +0300

    Time Series Entity + Cypress tests

--- core/libs/connection/src/lib/shape-connection-handler.ts
+++ core/libs/connection/src/lib/shape-connection-handler.ts
@@ -79,15 +79,11 @@ abstract class InheritanceConnector {
   ) {}
 
   public connect(parentMetaModel: BaseMetaModelElement, childMetaModel: BaseMetaModelElement, parentCell: mxCell, childCell: mxCell) {
-    if ((childMetaModel as any).extendedElement === parentMetaModel) {
-      this.notificationsService.info(
-        `${childMetaModel.name} already extends ${parentMetaModel.name}. Please remove the connection to create a new one.`,
-        null,
-        null,
-        5000
-      );
+    if (typeof parentMetaModel['isPredefined'] === 'function' && parentMetaModel['isPredefined']()) {
+      this.notificationsService.warning("A predefined element can't have a child");
       return;
     }
+
     (parentMetaModel as any).extendedElement = childMetaModel;
     this.checkAndRemoveExtendElement(parentCell);
     this.mxGraphService.assignToParent(childCell, parentCell);
